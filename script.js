fc493aafcba2e2ff84281a133ea42bcf7e5f8515
Rowan Dowling <145838134+rowan-codes@users.noreply.github.com>
2024-05-17 16:52:38 +0100

    Fixed typos and reworked GetHumanChoice
    Trying to resolve issue with humanChoice being undefined

--- script.js
+++ script.js
@@ -3,55 +3,61 @@ function getComputerChoice(min, max) {
     const maxFloored = Math.floor(max);
     let randomInt = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
     switch (randomInt) {
-         case 1:
-         computerChoice = 'rock';
-         break;
+        case 1:
+        computerChoice = 'rock';
+        break;
  
-         case 2:
-         computerChoice = 'paper';
-         break;
+        case 2:
+        computerChoice = 'paper';
+        break;
  
-         case 3:
-         computerChoice = 'scissors';
-         break;
+        case 3:
+        computerChoice = 'scissors';
+        break;
      }
  return computerChoice;
  }
  
  function getHumanChoice() {
-     let humanInput = window.prompt('Rock, paper, or scissors?');
-     let humanChoice = humanInput.toLowerCase();
-     if (humanChoice == 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
-         return humanChoice;
-     } 
-     else {
-         console.log('Valid answers are rock, paper, or scissors.');
-         getHumanChoice();
-         return;
-     }
- }
+    let humanInput = window.prompt('Rock, paper, or scissors?');
+    if (humanInput != null) {
+        if (humanInput.toLowerCase == 'rock' || 'paper' || 'scissors') {
+            let humanChoice = humanInput.toLowerCase();
+            return humanChoice;
+        } 
+        else {
+            console.log('Valid answers are rock, paper, or scissors.');
+            getHumanChoice();
+            return
+        }
+    }
+    else {
+        return
+    }
+}
  
  /* initializing variables */
  let humanScore = 0;
  let computerScore = 0;
+ let roundTotal = 0;
  
  function humanWon() {
-        humanScore++;
-        console.log('You won that round!');
-        return humanScore;
+    humanScore++;
+    console.log('You won that round!');
+    return humanScore;
  }
  
  function computerWon() {
-        computerScore++;
-        console.log('The computer won that round.');
-        return computerScore;
+    computerScore++;
+    console.log('The computer won that round.');
+    return computerScore;
  }
  
  function playRound (humanChoice, computerChoice) {
-        roundTotal++;
-        console.log(print(`You chose $humanChoice and the computer chose ${computerChoice}.`));
-        calculateRoundWinner(humanChoice, computerChoice);
-        return;
+    roundTotal++;
+    console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}.`);
+    calculateRoundWinner(humanChoice, computerChoice);
+    return;
  }
  
  function calculateRoundWinner(humanChoice, computerChoice) {
@@ -60,35 +66,35 @@ function getComputerChoice(min, max) {
         return;
     } 
      else if (((humanChoice == 'rock') && (computerChoice == 'scissors')) || ((humanChoice == 'scissors') && (computerChoice == 'paper')) || ((humanChoice == 'paper') && (computerChoice == 'rock'))) {
-         humanWon();
-         return;
+        humanWon();
+        return;
     }
      else {
-         computerWon();
-         return;
+        computerWon();
+        return;
     }
 }
  
  function announceWinner(humanScore, computerScore) {
-        if (humanScore == computerScore) {
-           console.log(`Wow, it is somehow still a draw! You both got ${humanScore}.`);
-           return;
-        } 
-        else if (humanScore > computerScore) {
-           console.log(`You won! Congratulations. Your score was ${humanScore} and theirs was ${computerScore}.`);
-           return;
-        } 
-        else {
-           console.log(`Aw, the computer beat you. Their score was ${computerScore} and yours was only ${humanScore}. Never mind.`);
-           return;
-        }
+    if (humanScore == computerScore) {
+        console.log(`Wow, it is somehow still a draw! You both got ${humanScore}.`);
+        return;
+    } 
+    else if (humanScore > computerScore) {
+        console.log(`You won! Congratulations. Your score was ${humanScore} and theirs was ${computerScore}.`);
+        return;
+    } 
+    else {
+        console.log(`Aw, the computer beat you. Their score was ${computerScore} and yours was only ${humanScore}. Never mind.`);
+        return;
+    }
  }
  
  /* play best of 5 rounds */
  for (let roundTotal = 0; roundTotal < 5; roundTotal++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice(1, 3);
-        playRound(humanSelection, computerSelection);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice(1, 3);
+    playRound(humanSelection, computerSelection);
  }
  
  /* finish game */
